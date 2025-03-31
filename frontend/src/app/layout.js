eca5bfab4df8600fbaea6cbd85e025176b8f773e
Tarush Agarwal <tarushagarwal2003@gmail.com>
2025-03-31 20:23:22 +0530

    added frontend

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: 'AudioMind',
+  description: 'Analyze your audio with AI.',
+  keywords: ['audio', 'AI', 'analysis', 'machine learning', 'audio processing'],
+  author: 'Tarush Agarwal',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
